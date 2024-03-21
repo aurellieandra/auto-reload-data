ed8662cd254366ed377b040d16fba57ae7321bc6
aurellieandra <diandra.azra@gmail.com>
2024-03-21 20:41:22 +0700

    finishing auto-reload-data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http"
 	"time"
@@ -8,28 +9,65 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Element struct {
+	Value int    `json:"value"`
+	Unit  string `json:"unit"`
+}
+
+type Data map[string]Element
+
 func main() {
-	g := gin.Default
+	g := gin.Default()
 
-	data := map[string]any{}
+	elementData := make(Data)
 
-	go func()  {
+	go func() {
 		for {
-			// read json file
-			// update json file
-			data["water"] = rand.Int31n(100)
-			data["wind"] = rand.Int31n(100)
-			data["fire"] = rand.Int31n(100)
-			data["earth"] = rand.Int31n(100)
+			elementData["water"] = Element{
+				Value: rand.Intn(100),
+				Unit:  "m",
+			}
+			elementData["wind"] = Element{
+				Value: rand.Intn(100),
+				Unit:  "m/s",
+			}
 			time.Sleep(15 * time.Second)
 		}
 	}()
 
-	g.GET("/", func(ctx *gin.Context)  {
-		ctx.JSON(http.StatusOK, map[string]any{
-			"status": data,
-		})
-	})
+	g.GET("/", func(ctx *gin.Context) {
+		var waterStatus string
+		var windStatus string
 
+		water := elementData["water"].Value
+		wind := elementData["wind"].Value
+
+		switch {
+		case water <= 50:
+			waterStatus = "aman"
+		case water >= 80:
+			waterStatus = "bahaya"
+		default:
+			waterStatus = "siaga"
+		}
+
+		switch {
+		case wind <= 60:
+			windStatus = "aman"
+		case wind >= 90:
+			windStatus = "bahaya"
+		default:
+			windStatus = "siaga"
+		}
+
+		response := map[string]interface{}{
+			"water":       fmt.Sprintf("%d %s", water, elementData["water"].Unit),
+			"waterStatus": waterStatus,
+			"wind":        fmt.Sprintf("%d %s", wind, elementData["wind"].Unit),
+			"windStatus":  windStatus,
+		}
+
+		ctx.JSON(http.StatusOK, response)
+	})
 	g.Run(":3000")
 }
